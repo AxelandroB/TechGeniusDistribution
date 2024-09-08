52165d0a58c9d19ea1463c738c4a15a94f7443c6
TERATERA-b-a-i-t-o <juansebastianvasquez06@gmail.com>
2024-09-08 19:15:53 -0300

    inicio de navbar en home

--- php/home.php
+++ php/home.php
@@ -3,9 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="../css/homestyle.css">
 </head>
 <body>
-    
+    <header>
+        <nav> 
+            <ul>
+                <li class="title">TechGenius Distribution S.A</li>
+                <li><a href = "#" >:v</a></li>
+                <li><a href = "#" >:v</a></li>
+                <li><a href = "#" >:v</a></li>
+                <li><a href = "#" >:v</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="information">
+        <p>
+        Nuestra empresa se dedica por completo a ofrecer productos electrónicos de alta calidad, 
+        ventas con pasión, conocimiento y un servicio técnico excepcional. Nos esforzamos por superar 
+        las expectativas de nuestros clientes en cada interacción, demostrando nuestro compromiso con 
+        la excelencia en todo lo que hacemos. Esta empresa estaría dirigida a clientes que valoran la 
+        calidad, la confiabilidad y el servicio al cliente excepcional en el ámbito de los productos y 
+        componentes electrónicos.
+
+        </p>
+
+    </div>
+
 </body>
 </html>
