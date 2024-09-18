d39cdd4ef69dce3bd7607cf082d840fd98b47959
Tiziano26 <132932851+Tiziano26@users.noreply.github.com>
2024-09-18 16:41:58 -0300

    asd

--- php/env.php
+++ php/env.php
@@ -1,8 +1,16 @@
 <?php
 
-$conexion=new PDO("sqlsrv:server=PC-F-16\SQLEXPRESS;database=Techgenius_Distribution_SA;","sa","123");
+$conexion=new PDO("sqlsrv:server=DESKTOP-RIVIFEH;database=Techgenius_Distribution_SA;","sa","1234");
 
 $consulta=$conexion -> prepare("SELECT * FROM Empleados");
 $consulta->execute();
 $datos=$consulta -> fetchAll(PDO::FETCH_ASSOC);
+
+// Imprimir los datos
+foreach ($datos as $fila) {
+    foreach ($fila as $clave => $valor) {
+        echo $clave . ': ' . $valor . '<br>';
+    }
+    echo '<hr>';
+}
 ?>
