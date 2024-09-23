b05cb867a41ada6457d0e2f3b48cc152e3610f45
Luis-Daniel-Espinoza-Gonzales <danidex129@gmail.com>
2024-09-22 23:13:12 -0300

    asd

--- php/env.php
+++ php/env.php
@@ -1,8 +1,17 @@
 <?php
 
-$conexion=new PDO("sqlsrv:server=PC-F-16\SQLEXPRESS;database=Techgenius_Distribution_SA;","sa","123");
+$conexion=new PDO("sqlsrv:server=DESKTOP-I20CH35\SQLEXPRESS;database=Techgenius_Distribution_SA;","sa","123");
 
 $consulta=$conexion -> prepare("SELECT * FROM Empleados");
 $consulta->execute();
 $datos=$consulta -> fetchAll(PDO::FETCH_ASSOC);
+
+// Imprimir los datos
+foreach ($datos as $fila) {
+    foreach ($fila as $clave => $valor) {
+        echo $clave . ': ' . $valor . '<br>';
+    }
+    echo '<hr>';
+}
+
 ?>
