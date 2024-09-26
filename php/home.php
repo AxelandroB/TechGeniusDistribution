dfd2f37f2e43d2795a9912a2082acb124fb996e2
TERATERA-b-a-i-t-o <juansebastianvasquez06@gmail.com>
2024-09-26 20:24:04 -0300

    diseño del home

--- php/home.php
+++ php/home.php
@@ -1,35 +1,115 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="stylesheet" type="text/css" href="../css/homestyle.css">
+    <link rel="stylesheet" type="text/css" href="http://localhost/Analisis_Sistemas/css/homestyle.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+
 </head>
+
 <body>
     <header>
-        <nav> 
+        <nav>
             <ul>
-                <li class="title">TechGenius Distribution S.A</li>
-                <li><a class = "opt" href = "#" >:v</a></li>
-                <li><a class = "opt" href = "#" >:v</a></li>
-                <li><a class = "opt" href = "#" >:v</a></li>
-                <li><a class = "opt" href = "#" >:v</a></li>
+                <li class="title">TechGenius Distribution <div class = "antitle">S</div><div class = "antitle">.</div><div class = "antitle">A</div></li>
+                <li><a id="notp" href="#">Texto</a></li>
+                <li><a id="notp2" href="#">Texto</a></li>
+                <li><a href="Perfil.php" id="pfp">Perfil</a></li>
             </ul>
         </nav>
+        <svg id="usericon" class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+            <path fill-rule="evenodd" d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" clip-rule="evenodd" />
+        </svg>
+
     </header>
-    <div class="information">
-        <p>
-        Nuestra empresa se dedica por completo a ofrecer productos electrónicos de alta calidad, 
-        ventas con pasión, conocimiento y un servicio técnico excepcional. Nos esforzamos por superar 
-        las expectativas de nuestros clientes en cada interacción, demostrando nuestro compromiso con 
-        la excelencia en todo lo que hacemos. Esta empresa estaría dirigida a clientes que valoran la 
-        calidad, la confiabilidad y el servicio al cliente excepcional en el ámbito de los productos y 
-        componentes electrónicos.
 
+    <h1><u>TechGenius Distribution S.A</u></h1>
+    <h4><u><i>(¿Que hace la empresa?)</i></u></h5>
+        <img src="../images/Elogo.png" alt="" width="550" height="550" class="logo">
+        <div class="information">
+
+            <p id = ftext>
+                Nuestra empresa se dedica por completo a ofrecer productos electrónicos de alta calidad,
+                ventas con pasión, conocimiento y un servicio técnico excepcional. Nos esforzamos por superar
+                las expectativas de nuestros clientes en cada interacción, demostrando nuestro compromiso con
+                la excelencia en todo lo que hacemos. Esta empresa estaría dirigida a clientes que valoran la
+                calidad, la confiabilidad y el servicio al cliente excepcional en el ámbito de los productos y
+                componentes electrónicos.
+            </p>
+
+        </div>
+        <div class="contenedor">
+        <p class = "objetivos">Nuestros objetivos como empresa distribuidora de componentes, Hardware y ofrecimiento de servicio técnico consiste en:
+            Brindar productos de hardware de alta calidad y confiabilidad a los clientes en nuestras sucursales ubicadas en CABA, además
+            ofrecemos un servicio técnico rápido y eficiente para solucionar los problemas de hardware de los clientes.Dentro del área interna
+            nosotros queremos optimizar los procesos de las distintas áreas que conforman nuestra empresa para mejorar la eficiencia y reducir
+            los costos operativos, también nos centramos en establecer alianzas estratégicas con fabricantes y proveedores para garantizar
+            la disponibilidad y calidad de los productos. Asimismo queremos desarrollar una sólida reputación en el mercado como una empresa
+            confiable y experta en hardware y servicios técnicos incluso alcanzar un crecimiento sostenible, exponencial y rentable en el
+            mercado de la tecnología.
         </p>
-
     </div>
+        <div class="decoration">
+            <svg class="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                <path fill="#73EC8B" fill-opacity="0.6" d="M0,160L8,170.7C16,181,32,203,48,192C64,181,80,139,96,128C112,117,128,139,144,144C160,149,176,139,192,128C208,117,224,107,240,85.3C256,64,272,32,288,53.3C304,75,320,149,336,192C352,235,368,245,384,256C400,267,416,277,432,272C448,267,464,245,480,224C496,203,512,181,528,160C544,139,560,117,576,112C592,107,608,117,624,149.3C640,181,656,235,672,240C688,245,704,203,720,176C736,149,752,139,768,149.3C784,160,800,192,816,213.3C832,235,848,245,864,245.3C880,245,896,235,912,208C928,181,944,139,960,122.7C976,107,992,117,1008,138.7C1024,160,1040,192,1056,192C1072,192,1088,160,1104,154.7C1120,149,1136,171,1152,197.3C1168,224,1184,256,1200,240C1216,224,1232,160,1248,112C1264,64,1280,32,1296,64C1312,96,1328,192,1344,208C1360,224,1376,160,1392,117.3C1408,75,1424,53,1432,42.7L1440,32L1440,320L1432,320C1424,320,1408,320,1392,320C1376,320,1360,320,1344,320C1328,320,1312,320,1296,320C1280,320,1264,320,1248,320C1232,320,1216,320,1200,320C1184,320,1168,320,1152,320C1136,320,1120,320,1104,320C1088,320,1072,320,1056,320C1040,320,1024,320,1008,320C992,320,976,320,960,320C944,320,928,320,912,320C896,320,880,320,864,320C848,320,832,320,816,320C800,320,784,320,768,320C752,320,736,320,720,320C704,320,688,320,672,320C656,320,640,320,624,320C608,320,592,320,576,320C560,320,544,320,528,320C512,320,496,320,480,320C464,320,448,320,432,320C416,320,400,320,384,320C368,320,352,320,336,320C320,320,304,320,288,320C272,320,256,320,240,320C224,320,208,320,192,320C176,320,160,320,144,320C128,320,112,320,96,320C80,320,64,320,48,320C32,320,16,320,8,320L0,320Z"></path>
+            </svg>
+            <svg class="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                <path fill="#73EC8B" fill-opacity="0.3" d="M0,160L8,170.7C16,181,32,203,48,192C64,181,80,139,96,128C112,117,128,139,144,144C160,149,176,139,192,128C208,117,224,107,240,85.3C256,64,272,32,288,53.3C304,75,320,149,336,192C352,235,368,245,384,256C400,267,416,277,432,272C448,267,464,245,480,224C496,203,512,181,528,160C544,139,560,117,576,112C592,107,608,117,624,149.3C640,181,656,235,672,240C688,245,704,203,720,176C736,149,752,139,768,149.3C784,160,800,192,816,213.3C832,235,848,245,864,245.3C880,245,896,235,912,208C928,181,944,139,960,122.7C976,107,992,117,1008,138.7C1024,160,1040,192,1056,192C1072,192,1088,160,1104,154.7C1120,149,1136,171,1152,197.3C1168,224,1184,256,1200,240C1216,224,1232,160,1248,112C1264,64,1280,32,1296,64C1312,96,1328,192,1344,208C1360,224,1376,160,1392,117.3C1408,75,1424,53,1432,42.7L1440,32L1440,320L1432,320C1424,320,1408,320,1392,320C1376,320,1360,320,1344,320C1328,320,1312,320,1296,320C1280,320,1264,320,1248,320C1232,320,1216,320,1200,320C1184,320,1168,320,1152,320C1136,320,1120,320,1104,320C1088,320,1072,320,1056,320C1040,320,1024,320,1008,320C992,320,976,320,960,320C944,320,928,320,912,320C896,320,880,320,864,320C848,320,832,320,816,320C800,320,784,320,768,320C752,320,736,320,720,320C704,320,688,320,672,320C656,320,640,320,624,320C608,320,592,320,576,320C560,320,544,320,528,320C512,320,496,320,480,320C464,320,448,320,432,320C416,320,400,320,384,320C368,320,352,320,336,320C320,320,304,320,288,320C272,320,256,320,240,320C224,320,208,320,192,320C176,320,160,320,144,320C128,320,112,320,96,320C80,320,64,320,48,320C32,320,16,320,8,320L0,320Z"></path>
+            </svg>
+            <svg class="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                <path fill="#73EC8B" fill-opacity="0.2" d="M0,160L8,170.7C16,181,32,203,48,192C64,181,80,139,96,128C112,117,128,139,144,144C160,149,176,139,192,128C208,117,224,107,240,85.3C256,64,272,32,288,53.3C304,75,320,149,336,192C352,235,368,245,384,256C400,267,416,277,432,272C448,267,464,245,480,224C496,203,512,181,528,160C544,139,560,117,576,112C592,107,608,117,624,149.3C640,181,656,235,672,240C688,245,704,203,720,176C736,149,752,139,768,149.3C784,160,800,192,816,213.3C832,235,848,245,864,245.3C880,245,896,235,912,208C928,181,944,139,960,122.7C976,107,992,117,1008,138.7C1024,160,1040,192,1056,192C1072,192,1088,160,1104,154.7C1120,149,1136,171,1152,197.3C1168,224,1184,256,1200,240C1216,224,1232,160,1248,112C1264,64,1280,32,1296,64C1312,96,1328,192,1344,208C1360,224,1376,160,1392,117.3C1408,75,1424,53,1432,42.7L1440,32L1440,320L1432,320C1424,320,1408,320,1392,320C1376,320,1360,320,1344,320C1328,320,1312,320,1296,320C1280,320,1264,320,1248,320C1232,320,1216,320,1200,320C1184,320,1168,320,1152,320C1136,320,1120,320,1104,320C1088,320,1072,320,1056,320C1040,320,1024,320,1008,320C992,320,976,320,960,320C944,320,928,320,912,320C896,320,880,320,864,320C848,320,832,320,816,320C800,320,784,320,768,320C752,320,736,320,720,320C704,320,688,320,672,320C656,320,640,320,624,320C608,320,592,320,576,320C560,320,544,320,528,320C512,320,496,320,480,320C464,320,448,320,432,320C416,320,400,320,384,320C368,320,352,320,336,320C320,320,304,320,288,320C272,320,256,320,240,320C224,320,208,320,192,320C176,320,160,320,144,320C128,320,112,320,96,320C80,320,64,320,48,320C32,320,16,320,8,320L0,320Z"></path>
+            </svg>
+        </div>
 
 </body>
+<script>
+    var user = document.getElementById('usericon');
+    document.getElementById('pfp').addEventListener('mouseover', on);
+    document.getElementById('pfp').addEventListener('mouseout', out);
+
+    function on() {
+        user.style.right = "370px";
+        user.style.filter = "invert(100)"
+    }
+
+    function out() {
+
+        user.style.right = "380px";
+        user.style.filter = "invert(0)"
+    }
+    document.getElementById('notp').addEventListener('mouseover', ses);
+    document.getElementById('notp').addEventListener('mouseout', nel);
+
+    function ses() {
+        user.style.right = "370px";
+        
+    }
+
+    function nel() {
+
+        user.style.right = "380px";
+   
+    }
+
+    document.getElementById('notp2').addEventListener('mouseover', ses);
+    document.getElementById('notp2').addEventListener('mouseout', nel);
+
+    function ses() {
+        user.style.right = "370px";
+        
+    }
+
+    function nel() {
+
+        user.style.right = "380px";
+   
+    }
+</script>
+
 </html>
