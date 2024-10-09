7a432e891f30a347699fe3f685c970e8eb8f317a
Luis-Daniel-Espinoza-Gonzales <danidex129@gmail.com>
2024-10-09 17:16:36 -0300

    subo navbar.php y navbar.css. hago include en home.php

--- php/home.php
+++ php/home.php
@@ -26,20 +26,9 @@ if (!isset($_SESSION['name'])) {
 </head>
 
 <body>
-    <header>
-        <nav>
-            <ul>
-                <li class="title">TechGenius Distribution <div class = "antitle">S</div><div class = "antitle">.</div><div class = "antitle">A</div></li>
-                <li><a id="notp" href="#">Texto</a></li>
-                <li><a id="notp2" href="#">Texto</a></li>
-                <li><a href="Perfil.php" id="pfp">Perfil</a></li>
-            </ul>
-        </nav>
-        <svg id="usericon" class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-            <path fill-rule="evenodd" d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" clip-rule="evenodd" />
-        </svg>
-
-    </header>
+    <?php
+        include "navbar.php";
+    ?>
 
     <h1><u>TechGenius Distribution S.A</u></h1>
     <h4><u><i>(¿Que hace la empresa?)</i></u></h5>
