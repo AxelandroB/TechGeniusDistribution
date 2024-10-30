5406c3164d4463a466875ee6a061209e05d65941
Luis-Daniel-Espinoza-Gonzales <danidex129@gmail.com>
2024-10-30 10:13:17 -0300

    subo tabla no dinamica

--- php/marketing.php
+++ php/marketing.php
@@ -1,55 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="../js/marketing.js"></script>
+    <link rel="stylesheet" href="../css/marketing.css">
     <title>Document</title>
 </head>
+
 <body onload="informacion()">
 
     <?php
-        include "navbar.php";
+    include "navbar.php";
     ?>
+    <div class="content">
 
-    <form action="php/funciones/ingresa_logistica.php" method="post">
-        <h2>Ingrese dato a agregar</h1>
+        <table>
+            <tr>
+                <td>ID</td>
+                <td>Fecha inicial</td>
+                <td>Fehca final</td>
+                <td>Colaboracion</td>
+                <td>Producto</td>
+                <td>Color</td>
+                <td>Marca</td>
+                <td>Tipo de producto</td>
+            </tr>
+        </table>
 
-            <label for="">Nro.Registro</label>
-            <br>
-            <input type="text" placeholder="Escriba el id:" name="ID">
-            <br>
-
-            <label for="">Empresa de transporte</label>
-            <br>
-            <input type="text" placeholder="Escriba id de empresa" name="Id_empresa">
-            <br>
-
-            <label for="">Transporte</label>
-            <br>
-            <input type="text" placeholder="Medio de transporte:" name="Transporte">
-            <br>
-
-            <label for="">Fecha de ingreso</label>
-            <br>
-            <input type="date" placeholder="Fecha de entrada" name="Fecha">
-            <br>
-
-            <label for="">Producto</label>
-            <br>
-            <input type="text" placeholder="Id producto" name="Producto">
-            <br>
-
-           
-
-            <label for="">Cantidad de productos</label>
-            <br>
-            <input type="text" placeholder="Unidades" name="Unidades">
-            <br>
-
-                <input type="submit" value="Ingresar">
-
-    </form>
+    </div>
 </body>
+
 </html>
